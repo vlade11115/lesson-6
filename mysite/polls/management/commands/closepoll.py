265c7cf08733428b6f8ea2b87e37c89df79d2a4a
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-04-21 21:23:37 +0300

    Final state

---
+++ mysite/polls/management/commands/closepoll.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+from polls.models import Question
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("questions", type=int)
+
+    def handle(self, *args, questions, **options, ):
+        for _ in range(questions):
+            q = Question.objects.create(question_text='42', pub_date=now())
+            self.stdout.write(
+                self.style.SUCCESS('Successfully created Question with ID "%s"' % q.id)
+            )
